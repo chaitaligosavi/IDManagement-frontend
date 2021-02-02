e7118e82089065cfabc5bb8cdcfdc7f39cf2da70
Chaitali Gosavi (Senior Executive <166159@fullertonindia.local>
2021-02-02 14:15:00 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'login', component: LoginComponent },
+    { path: 'login/:message', component: LoginComponent },
 
+    // { path: 'error', component: PageNotFoundComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full'}
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes,  { useHash: true } ) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule{ }
